0f6657392b405fc8773b0adf85ebf79248107128
Pedro Henrique <pedrohenrique8298@gmail.com>
2023-10-18 21:17:01 -0300

    Menu

---
+++ radar.js
@@ -0,0 +1,10 @@
+const box = document.querySelector('.box');
+const adicional = document.querySelector('.adicional');
+
+box.addEventListener('click', function() {
+    if (adicional.style.display === 'none') {
+        adicional.style.display = 'block'; // Mostra o conteúdo quando a div é clicada
+    } else {
+        adicional.style.display = 'none'; // Oculta o conteúdo se ele já estiver visível
+    }
+});
